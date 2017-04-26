5bcbfbe13c35265352bbafe83b25b201adbe8fc3
qiuchen90 <qiuchen@berkeley.edu>
2017-04-25 19:34:06 -0700

    Fluid -body interaction

---
+++ qiuchen_visualization/sph_type.h
@@ -0,0 +1,24 @@
+#ifndef __SPHTYPE_H__
+#define __SPHTYPE_H__
+
+typedef unsigned int uint;
+
+struct float2
+{
+	float x;
+	float y;
+};
+
+struct int2
+{
+	int x;
+	int y;
+};
+
+struct uint2
+{
+	uint x;
+	uint y;
+};
+
+#endif
