0964df8b4d8086267038ddedac4b67d70c463d8e
Lunj12 <lawrence12thu@gmail.com>
2017-05-01 23:41:09 -0400

    Add serial

---
+++ gpu_2/sph_header.h
@@ -0,0 +1,24 @@
+#ifndef __SPHHEADER_H__
+#define __SPHHEADER_H__
+
+#include <thrust/device_vector.h>
+#include <thrust/copy.h>
+#include <thrust/device_ptr.h>
+#include <thrust/for_each.h>
+#include <thrust/iterator/zip_iterator.h>
+#include <thrust/sort.h>
+#include <thrust/system_error.h>
+
+#include <vector>
+#include <list>
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <time.h>
+
+#define PI 3.141592f
+#define INF 1E-12f
+#define BOUNDARY 0.001f
+
+#endif
